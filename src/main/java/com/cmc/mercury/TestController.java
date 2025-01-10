cf02a6f30dab87b7d70dbf3ea88cb21b4678a7e3
dainnida <dain0928@g.hongik.ac.kr>
2025-01-10 13:50:23 +0900

    [CI/CD] 테스트 위한 controller 작성

---
+++ src/main/java/com/cmc/mercury/TestController.java
@@ -0,0 +1,13 @@
+package com.cmc.mercury;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello! CICD with Docker is completed";
+    }
+}
